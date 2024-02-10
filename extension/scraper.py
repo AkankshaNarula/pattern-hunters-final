991af860b12433d4d95c9285abe0844ee687bf45
Pranav Sharma <pranavsharma.bt22cse@pec.edu.in>
2024-02-10 19:35:44 +0530

    Removed unused files to save space

--- extension/scraper.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-driver = webdriver.Chrome()
-driver.get("https://amazon.in")
-print(driver.find_element(By.ID,"navbar-main").text)
-# print(html)
-time.sleep(6)
-driver.close()
--- extension/scraper2.py
+++
@@ -1,8 +0,0 @@
-import requests 
-from bs4 import BeautifulSoup 
-  
-URL = "https://amazon.in" 
-r = requests.get(URL) 
-  
-soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.prettify()) 
