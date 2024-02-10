364aff3bf64eb4c13a215665f9d580a444f7e2c4
Gaurav Gupta <123398315+GauravGupta993@users.noreply.github.com>
2024-02-10 12:26:20 +0000

    Made changes in extension and server according to multiclass model.

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,60 @@
-// const el=document.querySelectorAll("span.a-color-secondary");
-// .style.backgroundColor="blue"
-// const el=document.getElementsByClassName("s-main-slot");
-// const el2=el[0].querySelector("")
-// el[0].style.backgroundColor="red";
-// console.log(el);
+// const site = windows.location.hostname
+// console.log(site)
+
+// console.log("Injected to script: ")
+// alert("WORKS")
+(async ()=> {
+    // console.log("WORKS");
+    const response = await fetch("http://127.0.0.1:5000/data");
+    const dark_patterns = await response.json();
+    for(i in dark_patterns.result){
+        let text=dark_patterns.result[i][0];
+        let selector=dark_patterns.result[i][1].slice(0,-6);
+        let type=dark_patterns.result[i][3];
+        console.log(text+" "+selector+" "+type+"\n");
+        Change_CSS(selector,text,type);
+    }
+})();
+function Change_CSS(x,y,z) {
+    const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css
+    // let Selector = "span.a-badge-text";
+    Add_Custom_Style(`
+            .DPBH {
+                background-color: #008dff !important;
+            }
+            .DPBH:hover .DP-Type{
+                display:inline ;
+            }
+            .DP-Type{
+                z-index:99;
+                position:absolute;
+                display:none;
+                margin-left:50px;
+                color:white;
+                font-size: 20px;
+                border: 2px solid red;
+                padding-bottom: 10px;
+                padding-top: 5px;
+                padding-left: 2px;
+                background-color:red;
+            }
+        `)
+    // alert("Content script is working")
+    Selector = document.querySelectorAll(x)
+    for (var i = 0, len = Selector.length; i < len; i++) {
+        console.log(Selector[i].innerText)
+        if (Selector[i].innerText == y) {
+            console.log(Selector[i])
+            Selector[i].classList.add("DPBH")
+
+            const newNode = document.createElement("div");
+            const textNode = document.createTextNode(z);
+            newNode.appendChild(textNode);
+            newNode.classList.add("DP-Type")
+            const list = document.getElementById("myList");
+            Selector[i].insertBefore(newNode, Selector[i].children[0]);
+        }
+
+    }
+
+}
--- extension/eventPage.js
+++ extension/eventPage.js
@@ -1,4 +1,5 @@
 let link;
+let output;
 
 var menuItem = {
   "id": "Checktest",
@@ -20,14 +21,14 @@ chrome.contextMenus.onClicked.addListener(function (clickData) {
         body: JSON.stringify({ 'value': clickData.selectionText })
       });
       let content = await rawResponse.json();
-      console.log(content);
+      // console.log(content);
       var timestamp = new Date().getTime();
       chrome.notifications.create(
         "notification2" + timestamp,
         {
           type: "basic",
           iconUrl: "images.jpg",
-          title: "This is a notification",
+          title: "Dark Pattern Analysis Report",
           message: content.result,
         },
         function () { }
@@ -41,7 +42,7 @@ async function getCurrentTab() {
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
 
-  console.log(tab)
+  // console.log(tab)
   // notifyMe();
   // const notification= new Notification("Hi there!");
   if (typeof tab == 'undefined') {
@@ -52,7 +53,7 @@ async function getCurrentTab() {
         return;
       }
       link = tab.url;
-      console.log(tab.url);
+      // console.log(tab.url);
       (async () => {
         const rawResponse = await fetch('http://127.0.0.1:5000/url', {
           method: 'POST',
@@ -63,28 +64,33 @@ async function getCurrentTab() {
           body: JSON.stringify({ 'value': tab.url })
         });
         let content = await rawResponse.json();
-        console.log(content.result);
-        console.log(typeof (content.result))
-        // alert("hello")
-        // chrome.tabs.create({url:"dpbh.html"});
-        var timestamp = new Date().getTime();
-        chrome.notifications.create(
-          "notification" + timestamp,
-          {
-            type: "basic",
-            iconUrl: "images.jpg",
-            title: "This is a notification",
-            message: content.result,
-            // expandedMessage: "works"
-            // items: [{"1":result[0]}],
-
-          },
-          function () { }
-        );
+        console.log(content.result)
       })();
     }
   }
 }
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     // console.log(sender.tab ?
+//     //             "from a content script:" + sender.tab.url :
+//     //             "from the extension");
+//     if (request.greeting === "hello"){
+//       var content;
+//       content=chrome.tabs.onUpdated.addListener(async function (activeInfo) {
+//         // console.log(activeInfo);
+//         // console.log("SendResponse")
+//         content= await getCurrentTab();
+//         return content
+//         // chrome.tabs.create({url:"dpbh.html"});
+//       });
+//       console.log(content);
+//       sendResponse({output:"hello"});
+//       // sendResponse({farewell:"hello"});
+//     }
+//   }
+// );
+// console.log("Background Script")
 chrome.tabs.onUpdated.addListener(function (activeInfo) {
   console.log(activeInfo);
   getCurrentTab();
